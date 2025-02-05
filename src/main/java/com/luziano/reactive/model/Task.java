98507fe75749c63ead60f7da45ccb219862f90e1
Arthur Luziano <luzianoarthur@gmail.com>
2025-02-05 18:51:25 -0300

    Aplicação do padrão Builder.

--- src/main/java/com/luziano/reactive/model/Task.java
+++ src/main/java/com/luziano/reactive/model/Task.java
@@ -2,18 +2,73 @@ package com.luziano.reactive.model;
 
 import com.luziano.reactive.service.TaskService;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class Task {
     private String title;
     private String description;
     private int priority;
     private TaskState state;
 
+    public Task(Builder builder) {
+        this.title = builder.title;
+        this.description = builder.description;
+        this.priority = builder.priority;
+        this.state = builder.state;
+    }
+
+    public Task() {}
+
     public Task newTask() {
         TaskService.taskList.add(this);
         return this;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static Builder builderFrom(Task task) {
+        return new Builder(task);
+    }
+
+    public static class Builder {
+        private String title;
+        private String description;
+        private int priority;
+        private TaskState state;
+
+        public Builder(Task task) {
+            this.title = task.title;
+            this.description = task.description;
+            this.priority = task.priority;
+            this.state = task.state;
+        }
+
+        public Builder() {}
+
+        public Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder withPriority(int priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder withState(TaskState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(this);
+        }
     }
 }
