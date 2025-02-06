7664dae2e2480bd2ea95353c05f3aba47b119d8a
Arthur Luziano <arthur.luziano@araujo.com.br>
2025-02-06 13:54:43 -0300

    Adição teste unitário controller.

---
+++ src/test/java/com/luziano/reactive/controller/data/TaskControllerTestData.java
@@ -0,0 +1,96 @@
+package com.luziano.reactive.controller.data;
+
+import com.luziano.reactive.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+public class TaskControllerTestData {
+    public static Page<Task> page() {
+        return new Page<Task>() {
+            @Override
+            public int getTotalPages() {
+                return 0;
+            }
+
+            @Override
+            public long getTotalElements() {
+                return 0;
+            }
+
+            @Override
+            public <U> Page<U> map(Function<? super Task, ? extends U> converter) {
+                return null;
+            }
+
+            @Override
+            public int getNumber() {
+                return 0;
+            }
+
+            @Override
+            public int getSize() {
+                return 0;
+            }
+
+            @Override
+            public int getNumberOfElements() {
+                return 0;
+            }
+
+            @Override
+            public List<Task> getContent() {
+                return List.of();
+            }
+
+            @Override
+            public boolean hasContent() {
+                return false;
+            }
+
+            @Override
+            public Sort getSort() {
+                return null;
+            }
+
+            @Override
+            public boolean isFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isLast() {
+                return false;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return false;
+            }
+
+            @Override
+            public Pageable nextPageable() {
+                return null;
+            }
+
+            @Override
+            public Pageable previousPageable() {
+                return null;
+            }
+
+            @Override
+            public Iterator<Task> iterator() {
+                return null;
+            }
+        };
+    }
+}
