051cb87b32708fe7177d6057a0a26b4c44f5b667
Arthur Luziano <luzianoarthur@gmail.com>
2025-03-18 19:47:30 -0300

    Refinamento na TaskDTO para não exibir campos nulos.

--- src/main/java/com/luziano/reactive/model/dto/TaskDTO.java
+++ src/main/java/com/luziano/reactive/model/dto/TaskDTO.java
@@ -1,5 +1,6 @@
 package com.luziano.reactive.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.luziano.reactive.model.Address;
 import com.luziano.reactive.model.TaskState;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TaskDTO {
     private String id;
     private String title;
