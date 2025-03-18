6c48bbcfa5d4e78e24797acaa135f913a530d381
Arthur Luziano <luzianoarthur@gmail.com>
2025-03-18 19:44:31 -0300

    Implementação de re-tentativa em caso de task não encontrada.

--- src/main/java/com/luziano/reactive/service/TaskService.java
+++ src/main/java/com/luziano/reactive/service/TaskService.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -64,7 +65,12 @@ public class TaskService {
                 .flatMap(taskRepository::save)
                 .flatMap(producer::sendNotification)
                 .switchIfEmpty(Mono.error(TaskNotFoundException::new))
-                .doOnError(error -> log.error("Error on start task. ID: {} - ({})", id, LocalDateTime.now(), error));
+                .doOnError(error -> log.error("Error on start task. ID: {} - ({})", id, LocalDateTime.now(), error))
+                .retryWhen(Retry
+                        .backoff(3, Duration.ofSeconds(5))
+                        .maxBackoff(Duration.ofSeconds(15))
+                        .jitter(0.5)
+                        .filter(throwable -> throwable instanceof TaskNotFoundException));
     }
 
     public Mono<Task> done(Task task) {
